a0fb3483887587b68a13ed8e8a735710f150f2bd
spring <847193449@qq.com>
2020-01-03 16:25:13 +0800

    轮播

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Provider} from 'react-redux'
+import {IconStyle} from './assets/iconfont/iconfont';
+import {GlobalStyle} from './style';
+import {renderRoutes} from 'react-router-config';
+import store from './store/index'
+import routes from './routes/index.js';
+import {HashRouter} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <HashRouter>
+                <GlobalStyle></GlobalStyle>
+                <IconStyle></IconStyle>
+                {renderRoutes(routes)}
+            </HashRouter>
+        </Provider>
+    );
 }
 
 export default App;
